c6ee233315c9d1438180028a65df98fcd53a4e86
Денис Янбулатов <yanbulatov.denis@gmail.com>
2025-07-12 12:21:08 +0700

    add permutation func

--- functional_spec.v
+++ functional_spec.v
@@ -1,3 +1,5 @@
+From VST.floyd Require Import proofauto library.
+
 Require Import compcert.lib.Integers.
 Require Import compcert.lib.Zbits.
 Require Import compcert.lib.Coqlib.
@@ -62,3 +64,21 @@ Print Vec512.
 
 (* Инициализационный вектор для хэша 512 бит — все нули *)
 Definition IV512 : block512 := Vec512.repr 0.
+
+Definition p' : list Z :=
+  [0; 8; 16; 24; 32; 40; 48; 56;
+   1; 9; 17; 25; 33; 41; 49; 57;
+   2; 10; 18; 26; 34; 42; 50; 58;
+   3; 11; 19; 27; 35; 43; 51; 59;
+   4; 12; 20; 28; 36; 44; 52; 60;
+   5; 13; 21; 29; 37; 45; 53; 61;
+   6; 14; 22; 30; 38; 46; 54; 62;
+   7; 15; 23; 31; 39; 47; 55; 63].
+
+
+
+Fixpoint p (perm : list Z) (l : list byte) : list byte :=
+  match perm with
+  | [] => []
+  | i :: ps => (nth (Z.to_nat i) l default) :: (p ps l)
+  end.
