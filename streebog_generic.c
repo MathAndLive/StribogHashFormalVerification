8ddb82c3545957199ce01333b20bd19fa68259f5
Ann Hovanskaya <hovanja2011@gmail.com>
2025-07-14 14:28:58 +0300

    streebog_generic added

--- streebog_generic.c
+++ streebog_generic.c
@@ -1,20 +1,68 @@
-static const struct streebog_uint512 buffer0;
-static const struct streebog_uint512 buffer512;
-static const struct streebog_uint512 C[12];
-static const unsigned long long Ax[8][256];	
+From VST.floyd Require Import proofauto library.
+Require Import streebog_xor.
 
-static void streebog_xor;
-static void streebog_xlps;
-static void streebog_round;
-static int streebog_init;
-static void streebog_add512;
-static void streebog_g;
-static void streebog_stage2;
-static void streebog_stage3;
-static int streebog_update;
-static int streebog_finup;
+#[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
+Definition Vprog : varspecs. mk_varspecs prog. Defined.
+Require Import functional_spec.
 
-static struct shash_alg algs[2];
+Import ListNotations.
 
-static int __init streebog_mod_init(void);
-static void __exit streebog_mod_fini(void);
+(*
+struct streebog_uint512 {
+	__le64 qword[8];
+};
+
+void streebog_xor(const struct streebog_uint512 *x,
+			 const struct streebog_uint512 *y,
+			 struct streebog_uint512 *z)
+{
+	z->qword[0] = x->qword[0] ^ y->qword[0];
+	z->qword[1] = x->qword[1] ^ y->qword[1];
+	z->qword[2] = x->qword[2] ^ y->qword[2];
+	z->qword[3] = x->qword[3] ^ y->qword[3];
+	z->qword[4] = x->qword[4] ^ y->qword[4];
+	z->qword[5] = x->qword[5] ^ y->qword[5];
+	z->qword[6] = x->qword[6] ^ y->qword[6];
+	z->qword[7] = x->qword[7] ^ y->qword[7];
+}
+*)
+
+Definition t_streebog_uint512_st := Tstruct _streebog_uint512 noattr.
+Definition streebog_uint512_arr_length := 8.
+
+Check _streebog_uint512.
+Print t_streebog_uint512_st.
+
+Definition streebog_xor_spec :=
+  DECLARE _streebog_xor (* название верифицируемой функции из сгенерированного файла .v*)
+  WITH sh_r : share, sh_w : share, x : val, y : val, z : val,
+            x_content : block512, y_content : block512,
+            z_content : block512
+            (* share = тип доступа к памяти: read/write *)
+  PRE [tptr t_streebog_uint512_st, tptr t_streebog_uint512_st,
+       tptr t_streebog_uint512_st]
+       (* tptr - указатель на ... *)
+    PROP(readable_share sh_r; writable_share sh_w;
+         Zlength (block512_to_int64s x_content) = 8;
+         Zlength (block512_to_int64s y_content) = 8;
+         Zlength (block512_to_int64s z_content) = 8)
+    PARAMS (x; y; z) (* аргументы верифицируемой функции на C *)
+    SEP (field_at sh_r t_streebog_uint512_st (DOT _qword)
+            (map Vlong (block512_to_int64s x_content)) x;
+         field_at sh_r t_streebog_uint512_st (DOT _qword)
+            (map Vlong (block512_to_int64s y_content)) y;
+         field_at sh_w t_streebog_uint512_st (DOT _qword)
+            (map Vlong (block512_to_int64s z_content)) z)
+  POST [tvoid]
+    PROP()
+    RETURN()
+    SEP (field_at sh_r t_streebog_uint512_st (DOT _qword)
+            (map Vlong (block512_to_int64s x_content)) x;
+         field_at sh_r t_streebog_uint512_st (DOT _qword)
+            (map Vlong (block512_to_int64s y_content)) y;
+         field_at sh_w t_streebog_uint512_st (DOT _qword)
+            (map Vlong (block512_to_int64s (Vec512.xor x_content y_content))) z).
+
+
+
+Forall (fun w => 0 <= w < two_p 64) x_arr;
