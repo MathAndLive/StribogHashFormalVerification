b8aae73848b2f53b89907d2d65170cf3e379f56e
Ann Hovanskaya <hovanja2011@gmail.com>
2025-07-16 10:28:23 +0300

    proof with error

--- spec_streebog_xlps.v
+++ spec_streebog_xlps.v
@@ -1,5 +1,5 @@
 From VST.floyd Require Import proofauto library.
-Require Import streebog_xlps.
+Require Import streebog_generic.
 
 #[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
@@ -7,28 +7,7 @@ Require Import functional_spec.
 
 Import ListNotations.
 
-(*
-struct streebog_uint512 {
-	__le64 qword[8];
-};
-
-void streebog_xor(const struct streebog_uint512 *x,
-			 const struct streebog_uint512 *y,
-			 struct streebog_uint512 *z)
-{
-	z->qword[0] = x->qword[0] ^ y->qword[0];
-	z->qword[1] = x->qword[1] ^ y->qword[1];
-	z->qword[2] = x->qword[2] ^ y->qword[2];
-	z->qword[3] = x->qword[3] ^ y->qword[3];
-	z->qword[4] = x->qword[4] ^ y->qword[4];
-	z->qword[5] = x->qword[5] ^ y->qword[5];
-	z->qword[6] = x->qword[6] ^ y->qword[6];
-	z->qword[7] = x->qword[7] ^ y->qword[7];
-}
-*)
-
 Definition tuint512 := Tstruct _streebog_uint512 noattr.
-
 
 Definition streebog_xlps_spec :=
   DECLARE _streebog_xlps
@@ -36,9 +15,7 @@ Definition streebog_xlps_spec :=
          x : val, y : val, z : val,
          x_content : block512, y_content : block512, z_content : block512
   PRE [tptr tuint512, tptr tuint512, tptr tuint512]
-    PROP(readable_share sh_r; writable_share sh_w;
-         Zlength (block512_to_int64s x_content) = 8;
-         Zlength (block512_to_int64s y_content) = 8)
+    PROP(readable_share sh_r; writable_share sh_w)
     PARAMS (x; y; z)
     SEP (field_at sh_r tuint512 (DOT _qword) (map Vlong (block512_to_int64s x_content)) x;
          field_at sh_r tuint512 (DOT _qword) (map Vlong (block512_to_int64s y_content)) y;
@@ -55,24 +32,13 @@ Lemma body_streebog_xlps :
   semax_body Vprog [] f_streebog_xlps streebog_xlps_spec.
 Proof.
   start_function.
+  assert (Zlength (block512_to_int64s x_content) = 8) by reflexivity.
+  assert (Zlength (block512_to_int64s y_content) = 8) by reflexivity.
 
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 0 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 1 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 2 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 3 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 4 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 5 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 6 *)
-  forward. forward. forward. (* утверждения, что в массивах xy_content есть индекс 7 *)
+  do 24 forward.
 
-    (* forward_while
-    (EX i,
-      PROP (0 <= i <= 7)
-     *)
   forward.
-  assert (0 <= 0 < Zlength (block512_to_int64s x_content))
-    by (rewrite (Zlength (field)); simpl; lia).
-  forward.
+
   (* assert_PROP (Zlength (block512_to_int64s x_content) = 8). *)
 
 Qed.
