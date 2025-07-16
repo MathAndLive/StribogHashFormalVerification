8d1fb640f7ebc483be289f2becad47b270bed863
Ann Hovanskaya <hovanja2011@gmail.com>
2025-07-16 13:54:39 +0300

    first version of forward_loop

--- spec_streebog_xlps.v
+++ spec_streebog_xlps.v
@@ -34,11 +34,22 @@ Proof.
   start_function.
   assert (Zlength (block512_to_int64s x_content) = 8) by reflexivity.
   assert (Zlength (block512_to_int64s y_content) = 8) by reflexivity.
+  assert (Zlength (block512_to_int64s z_content) = 8) by reflexivity.
 
   do 24 forward.
 
-  forward.
-
-  (* assert_PROP (Zlength (block512_to_int64s x_content) = 8). *)
+  deadvars!.
+  forward_loop
+   (EX i:Z, EX j:Z, EX data: list int64,
+    PROP (0 <= i <= 8 ;
+          0 <= j <  i ;
+          nth (Z.to_nat j) data default =
+            nth (Z.to_nat j) (block512_to_int64s (LPSX x_content y_content)) Int64.zero)
+    LOCAL( )
+    SEP()
+    ).
+  - forward.
+    entailer!.
+  hint.
 
 Qed.
