59a8a7b99470300ff5cb8d992ce7b5875c140187
Vadim Marchenko <jakosvadim@gmail.com>
2025-07-17 08:02:26 +0300

    Add incomplite hoare triple

--- spec_streebog_xlps.v
+++ spec_streebog_xlps.v
@@ -17,16 +17,50 @@ Definition streebog_xlps_spec :=
   PRE [tptr tuint512, tptr tuint512, tptr tuint512]
     PROP(readable_share sh_r; writable_share sh_w)
     PARAMS (x; y; z)
-    SEP (field_at sh_r tuint512 (DOT _qword) (map Vlong (block512_to_int64s x_content)) x;
-         field_at sh_r tuint512 (DOT _qword) (map Vlong (block512_to_int64s y_content)) y;
-         field_at sh_w tuint512 (DOT _qword) (map Vlong (block512_to_int64s z_content)) z)
+    SEP (data_at sh_r tuint512 (map Vlong (block512_to_int64s x_content)) x;
+         data_at sh_r tuint512 (map Vlong (block512_to_int64s y_content)) y;
+         data_at sh_w tuint512 (map Vlong (block512_to_int64s z_content)) z)
   POST [tvoid]
     PROP()
     RETURN()
-    SEP (field_at sh_r tuint512 (DOT _qword) (map Vlong (block512_to_int64s x_content)) x;
-         field_at sh_r tuint512 (DOT _qword) (map Vlong (block512_to_int64s y_content)) y;
-         field_at sh_w tuint512 (DOT _qword)
+    SEP (data_at sh_r tuint512 (map Vlong (block512_to_int64s x_content)) x;
+         data_at sh_r tuint512 (map Vlong (block512_to_int64s y_content)) y;
+         data_at sh_w tuint512
            (map Vlong (block512_to_int64s (LPSX x_content y_content))) z).
+
+Definition inv_data_mem (sh_r : share) (i : Z) (x : val) (x_content : block512) : mpred :=
+  (data_at sh_r tuint512 (map Vlong (block512_to_int64s x_content)) x).
+
+Definition inv_data_mem_result (sh_w : share) (i : Z) (z : val) (x_content y_content z_content : block512) : mpred :=
+  (data_at sh_w tuint512 (map Vlong ((sublist 0 i (block512_to_int64s
+    (LPSX x_content y_content)) ++ (sublist i 8 (block512_to_int64s z_content))))) z).
+
+(* SEP (inv_data_mem i x y z x_content y_content z_content) *)
+
+     (* PROP (0 <= i <= 8; *)
+     (*       sublist 0 i (block512_to_int64s (LPSX x_content y_content)) = *)
+     (*        sublist 0 i (block512_to_int64s z_content)) *)
+     (* LOCAL (temp _x x; *)
+     (*        temp _y y; *)
+     (*        temp _z z) *)
+     (* SEP (data_at sh_r tuint512 (map Vlong (block512_to_int64s x_content)) x; *)
+     (*       data_at sh_r tuint512 (map Vlong (block512_to_int64s y_content)) y; *)
+     (*       data_at sh_w tuint512 (map Vlong (sublist 0 i (block512_to_int64s *)
+     (*        (LPSX x_content y_content)) ++ (sublist i 8 (block512_to_int64s z_content)))) z) *)
+
+Definition sumarray_Inv sh_r sh_w x y z x_content y_content z_content i :=
+   (PROP (0 <= i <= 8;
+           sublist 0 i (block512_to_int64s (LPSX x_content y_content)) =
+            sublist 0 i (block512_to_int64s z_content))
+     LOCAL (temp _i (Vint (Int.repr i));
+            temp _x x;
+            temp _y y;
+            temp _z z)
+     SEP (data_at sh_r tuint512 (map Vlong (block512_to_int64s x_content)) x;
+           data_at sh_r tuint512 (map Vlong (block512_to_int64s y_content)) y;
+           data_at sh_w tuint512 (map Vlong (sublist 0 i (block512_to_int64s
+            (LPSX x_content y_content)) ++ (sublist i 8 (block512_to_int64s z_content)))) z)
+   ).
 
 Lemma body_streebog_xlps :
   semax_body Vprog [] f_streebog_xlps streebog_xlps_spec.
@@ -40,16 +74,16 @@ Proof.
 
   deadvars!.
   forward_loop
-   (EX i:Z, EX j:Z, EX data: list int64,
-    PROP (0 <= i <= 8 ;
-          0 <= j <  i ;
-          nth (Z.to_nat j) data default =
-            nth (Z.to_nat j) (block512_to_int64s (LPSX x_content y_content)) Int64.zero)
-    LOCAL( )
-    SEP()
-    ).
-  - forward.
+  (EX i:Z,
+    PROP (sublist 0 i (block512_to_int64s (LPSX x_content y_content)) =
+          sublist 0 i (block512_to_int64s z_content))
+     LOCAL ()
+      SEP (inv_data_mem sh_r i x x_content;
+           inv_data_mem sh_r i y y_content;
+           inv_data_mem_result sh_w i z x_content y_content z_content)).
+  - forward. 
     entailer!.
-  hint.
-
+    Exists 0.
+    entailer!.
+  -
 Qed.
