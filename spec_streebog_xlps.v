aff6b87a54c87b52c826407643ccf578ea332b5d
Ann Hovanskaya <hovanja2011@gmail.com>
2025-07-18 12:26:04 +0300

    Ax created

--- spec_streebog_xlps.v
+++ spec_streebog_xlps.v
@@ -2,6 +2,10 @@ From VST.floyd Require Import proofauto library.
 Require Import streebog_generic.
 Require Import compcert.lib.Zbits.
 
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+
 #[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
 Require Import functional_spec.
@@ -36,35 +40,52 @@ Definition streebog_xlps_spec :=
 
 Definition inv_data_mem (i : Z) (x y z : val) (x_content y_content z_content : block512) : mpred :=
   (data_at Ews tuint512 (block512_to_vals x_content) x &&
-   data_at Ews tuint512 (block512_to_vals x_content) y &&
+   data_at Ews tuint512 (block512_to_vals y_content) y &&
    data_at Ews tuint512 (block512_to_vals (int64s_to_block512
       (sublist 0 i (block512_to_int64s (LPSX x_content y_content)) ++
                             (sublist i 8 (block512_to_int64s z_content))))) z).
 
-(*
-Definition A_rev := rev A.
 
-Fixpoint calc_a (A : list int64) (t : Z) (i : nat) (a : int64): int64 :=
-  match i with
-  | O => a
-  | S i' => match A with
+Definition A_Z := map Int64.unsigned A.
+Definition A_rev := rev A.
+Definition A_rev_Z := map Int64.unsigned A_rev.
+
+Fixpoint dec_to_bin (t : Z) (n : nat): list Z :=
+  match n with
+  | O => []
+  | S n' => Z_mod_two_p t 1 :: dec_to_bin (Z.shiftr t 1) n'
+  end.
+
+Compute Z_mod_two_p 252 1.
+Compute dec_to_bin 252 8.
+
+Fixpoint calc_a (A : list Z) (t : Z) (a : int64): int64 :=
+  match A with
     | [] => a
     | x :: xs => let x' := Z_mod_two_p t 1 * x
-      in calc_a xs (Z.shiftr 1 t) i' (Int64.xor a x)
-    end
+      in calc_a xs (Z.shiftr t 1) (Int64.xor a (Int64.repr x'))
   end.
 
-Fixpoint create_Ax (pi : list byte) (A : list int64) (i : Z) : list int64 :=
-  match i with
+
+Definition create_Ax_line (A : list Z) : list int64 :=
+  map (fun t => calc_a A t Int64.zero) (map Byte.unsigned pi').
+
+Fixpoint create_Ax (A : list Z) (j : nat) : list (list int64) :=
+  match j with
   | O => []
-  | S i' =>
-    match pi with
-    | [] => []
-    | x :: xs =>
-    end
+  | S j' => create_Ax_line (firstn 8 A) :: create_Ax (skipn 8 A) j'
   end.
 
-Fixpoint create_Ax (pi : list byte) (A : list int64) : list (list int64) . *)
+
+Definition Ax : list (list int64) := create_Ax A_rev_Z 8.
+
+Compute Ax.
+
+Compute 0xd01f715b5c7ef8e6. (*14996829921374828774*)
+Compute 0x16fa240980778325. (*1655675436240700197*)
+Compute 0xa8a42e857ee049c8. (*12151888845446597064*)
+(* ... *)
+Compute 0xd6a30f258c153427. (*15466222199258821671*)
 
 
 
