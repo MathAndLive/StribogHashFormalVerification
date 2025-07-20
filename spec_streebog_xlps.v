f3b0cc4cd32885154bb05eca45eaa679854fe565
Vadim Marchenko <jakosvadim@gmail.com>
2025-07-20 21:51:36 +0300

    Update streebog_xlps spec

--- spec_streebog_xlps.v
+++ spec_streebog_xlps.v
@@ -1,5 +1,6 @@
 From VST.floyd Require Import proofauto library.
 Require Import streebog_generic.
+Require Import spec_streebog_xor.
 
 #[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
@@ -33,12 +34,12 @@ Example test5: (Znth 1 (Znth 1 gen_Ax)) = Int64.repr 0x1906b59631b4f565.
 Proof. reflexivity. Qed.
 
 
-Definition block512_to_chunks (b : block512) : list Z :=
-  Z_to_chunks 64 8 (Vec512.unsigned b).
-
-Definition block512_to_vals (b : block512) : list val :=
-  map (fun x => Vlong (Int64.repr x)) (block512_to_chunks b).
-
+(* Definition block512_to_chunks (b : block512) : list Z := *)
+(*   Z_to_chunks 64 8 (Vec512.unsigned b). *)
+(**)
+(* Definition block512_to_vals (b : block512) : list val := *)
+(*   map (fun x => Vlong (Int64.repr x)) (block512_to_chunks b). *)
+(**)
 
 Definition data_mem (sh_r : share) (x : val) (x_content : block512) : mpred :=
   data_at sh_r tuint512 (block512_to_vals x_content) x.
@@ -51,13 +52,72 @@ Definition result_data_mem (sh_w : share) (i : Z) (z : val)
 Definition Ax_data_mem (sh_r : share) (Ax : val) : mpred :=
    data_at sh_r (tarray (tarray tlong 256) 8) (map (map Vlong) gen_Ax) Ax.
 
+(* Definition r_i_expr (i : Z) (j : Z) (x_content y_content : block512) : val := *)
+(*   Vlong (Int64.repr (Z.shiftr (Znth j (block512_to_chunks (Vec512.xor x_content y_content))) (64 * i))). *)
+
+Definition r_i_expr (i : Z) (j : Z) (x_content y_content : block512) : val :=
+  Vlong (Int64.repr (Znth j (block512_to_chunks (Vec512.xor x_content y_content)))).
+
+Fixpoint Z_to_xor_list (k : nat) (x y : Z) : list val :=
+  match k with
+  | O => Vlong (Int64.xor (Int64.repr (LSB 64 x)) (Int64.repr (LSB 64 y))) :: nil
+  | S k' => Vlong (Int64.xor (Int64.repr (LSB 64 x)) (Int64.repr (LSB 64 y))) ::
+            (Z_to_xor_list k' (Z.shiftr x (Z.of_nat 64)) (Z.shiftr y (Z.of_nat 64)))
+  end.
+
+Lemma r_i_eq : forall (i j : Z) (r1 r2 : block512),
+  Zlength (block512_to_chunks r1) = 8 -> Zlength (block512_to_chunks r2) = 8 ->
+  r_i_expr i j r1 r2 = Znth j (Z_to_xor_list 8 (Vec512.unsigned r1) (Vec512.unsigned r2)).
+Proof.
+  intros i j r1 r2 H1 H2.
+  (* intros i j r1 r2. *)
+  unfold r_i_expr.
+  unfold Z_to_xor_list.
+  rewrite !Z.shiftr_shiftr.
+  unfold block512_to_chunks.
+  unfold Z_to_chunks.
+  rewrite !xor_repr_comm.
+  rewrite !xor_LSB_comm.
+  Search (Z.lxor).
+  rewrite <- !Z.shiftr_lxor.
+  rewrite !Z.shiftr_shiftr.
+  Search (Znth).
+  Search map.
+  (* induction j as [|j' Hj']. *)
+  (* - simpl. *)
+  (*   unfold block512_to_chunks in *. *)
+  (*   unfold Z_to_chunks in *. *)
+  (*   rewrite Znth_0_cons. *)
+  (*   induction i as [|i' Hi']. *)
+  (*   + rewrite Znth_0_cons. *)
+  (*     rewrite Z.shiftr_0_r. *)
+  (*     rewrite xor_repr_comm. *)
+  (*     rewrite xor_LSB_comm. *)
+  (*     rewrite xor_unsigned_comm. *)
+  (*     reflexivity. *)
+  (*   + rewrite Nat2Z.inj_succ.  *)
+  (*     Search Zlength. *)
+  (*     Search (Znth). *)
+  (* unfold Z_to_xor_list. *)
+  (* unfold block512_to_chunks, Z_to_chunks. *)
+  (* rewrite Z.shiftr_0_r. *)
+
+  (* split. *)
+  (* +  *)
+  (*   rewrite xor_repr_comm. *)
+  (*   rewrite xor_LSB_comm. *)
+  (*   rewrite xor_unsigned_comm. *)
+  (*   unfold block512_to_chunks, Z_to_chunks. *)
+  (*   rewrite Znth_0_cons. *)
+  (*   reflexivity. *)
+Admitted.
 
 Definition streebog_xlps_spec :=
   DECLARE _streebog_xlps
   WITH   sh_r : share, sh_w : share,
          x : val, y : val, z : val,
          x_content : block512, y_content : block512, z_content : block512,
-         Ax: val
+         Ax : val, gv : globals
   PRE [tptr tuint512, tptr tuint512, tptr tuint512]
     PROP(readable_share sh_r; writable_share sh_w)
     PARAMS (x; y; z)
@@ -80,6 +140,7 @@ Definition streebog_xlps_spec :=
         Ax_data_mem sh_r Ax).
         (* data_at sh_w tuint512 (block512_to_vals (LPSX x_content y_content)) z; *)
      (* data_at sh_r (tarray (tarray tlong 256) 8) (map (map Vlong) gen_Ax) Ax). *)
+
 
 Lemma body_streebog_xlps :
   semax_body Vprog [] f_streebog_xlps streebog_xlps_spec.
@@ -108,15 +169,116 @@ Proof.
      PROP (0 <= i <= 8;
           sublist 0 i (block512_to_vals (LPSX x_content y_content)) =
           sublist 0 i (block512_to_vals z_content))
-     LOCAL (temp _i (Vint (Int.repr i)))
+     LOCAL (temp _i (Vint (Int.repr i));
+            temp _r0 (r_i_expr i 0 x_content y_content);
+            temp _r1 (r_i_expr i 1 x_content y_content);
+            temp _r2 (r_i_expr i 2 x_content y_content);
+            temp _r3 (r_i_expr i 3 x_content y_content);
+            temp _r4 (r_i_expr i 4 x_content y_content);
+            temp _r5 (r_i_expr i 5 x_content y_content);
+            temp _r6 (r_i_expr i 6 x_content y_content);
+            temp _r7 (r_i_expr i 7 x_content y_content);
+            gvars gv
+     )
      SEP (data_mem sh_r x x_content;
           data_mem sh_r y y_content;
           result_data_mem sh_w i z x_content y_content z_content;
-          Ax_data_mem sh_r Ax)).
+          Ax_data_mem sh_r (gv _Ax))).
   - forward. 
     entailer!.
     Exists 0.
     entailer!.
+
+    
+    split.
+    + rewrite r_i_eq; try assumption.
+      unfold Z_to_xor_list.
+      rewrite Znth_0_cons.
+      reflexivity.
+    + split; try lia.
+      ++ rewrite r_i_eq; try assumption.
+        unfold Z_to_xor_list.
+        rewrite Znth_pos_cons; try lia.
+        rewrite Znth_0_cons.
+        reflexivity.
+      ++  split.
+         +++ rewrite r_i_eq; try assumption.
+             unfold Z_to_xor_list.
+             rewrite 2!Znth_pos_cons; try lia.
+             rewrite Znth_0_cons.
+             reflexivity.
+         +++ split.
+             ++++ rewrite r_i_eq; try assumption.
+                 unfold Z_to_xor_list.
+                 rewrite 3!Znth_pos_cons; try lia.
+                 rewrite Znth_0_cons.
+                 reflexivity.
+             ++++ split.
+                  +++++ rewrite r_i_eq; try assumption.
+                     unfold Z_to_xor_list.
+                     rewrite 4!Znth_pos_cons; try lia.
+                     rewrite Znth_0_cons.
+                     reflexivity.
+                  +++++ split.
+                        -- rewrite r_i_eq; try assumption.
+                         unfold Z_to_xor_list.
+                         rewrite 5!Znth_pos_cons; try lia.
+                         rewrite Znth_0_cons.
+                         reflexivity.
+                        -- split.
+                           ** rewrite r_i_eq; try assumption.
+                             unfold Z_to_xor_list.
+                             rewrite 6!Znth_pos_cons; try lia.
+                             rewrite Znth_0_cons.
+                             reflexivity.
+                           ** split. 
+                              *** rewrite r_i_eq; try assumption.
+                               unfold Z_to_xor_list.
+                               rewrite 7!Znth_pos_cons; try lia.
+                               rewrite Znth_0_cons.
+                               reflexivity.
+                              ***
+  - Intros i.
+    forward_if.
+    forward.
+
+    + rewrite (r_i_eq 0 0 x_content y_content). intros Hr1.
+    unfold r_i_expr.
+    (* unfold block512_to_chunks, Z_to_chunks. *)
+    (* rewrite Z.shiftr_0_r. *)
+    split.
+    + 
+      rewrite xor_repr_comm.
+      rewrite xor_LSB_comm.
+      rewrite xor_unsigned_comm.
+      unfold block512_to_chunks, Z_to_chunks.
+      rewrite Znth_0_cons.
+      reflexivity.
+    + 
+      rewrite !xor_repr_comm.
+      rewrite !xor_LSB_comm.
+      rewrite <- !Z.shiftr_lxor.
+      rewrite !xor_unsigned_comm.
+      unfold block512_to_chunks, Z_to_chunks.
+      rewrite Znth_pos_cons.
+      split.
+      ++ rewrite Znth_0_cons.
+         reflexivity.
+      ++ rewrite 2!Znth_pos_cons.
+         split.
+         rewrite Znth_0_cons. 
+         +++ reflexivity.
+         +++ reflexivity.
+         +++ reflexivity.
+      ** reflexivity.
+      Search Znth.
+      Search Znth.
+
+      
+    Search (Znth ?y ?x = ?x.
+    unfold block512_to_chunks.
+    unfold Z_to_chunks.
+    rewrite Znth_list_repeat_inrange.
     (* unfold result_data_mem. entailer!. rewrite (sublist_same 0 8). *)
     (* autorewrite with sublist. entailer!. *)
   - Intros i.
