fd88176f59cf5c4345fc29cd0a14f69d9a4f1f65
Денис Янбулатов <yanbulatov.denis@gmail.com>
2025-07-23 17:41:37 +0700

    xpls attempts

--- spec_streebog_xlps.v
+++ spec_streebog_xlps.v
@@ -2,6 +2,7 @@ From VST.floyd Require Import proofauto library.
 Require Import streebog_generic.
 Require Import compcert.lib.Zbits.
 
+(* Definition Spec_streebog_xlps_VarSpecs : varspecs := (_Ax, (tarray (tarray tulong 256) 8))::nil. *)
 #[export] Instance CompSpecs : compspecs. make_compspecs prog. Defined.
 Definition Vprog : varspecs. mk_varspecs prog. Defined.
 Require Import functional_spec.
@@ -24,10 +25,19 @@ Definition Ax_to_vals (a : list (list int64)) : list (list val) :=
   map (fun i => map (fun j => Vlong j) i) a.
 
 
-Definition r_i_expr (i : Z) (j : Z) (x_content y_content : block512) : val :=
+Definition r_i_expr (j : Z) (x_content y_content : block512) : val :=
   Vlong (Int64.repr (Znth j (block512_to_chunks (Vec512.xor x_content y_content)))).
 
+(* Compute (Vec512.xor (Vec512.repr (2^128 -1)) (Vec512.repr 0)).
 
+Compute (block512_to_chunks (Vec512.xor (Vec512.repr (2^128 -1)) (Vec512.repr 0))).
+
+Compute (Znth 1 (block512_to_chunks (Vec512.xor (Vec512.repr (2^128 -1)) (Vec512.repr 0)))).
+
+Compute Vlong ((Int64.repr (Znth 3 (block512_to_chunks (Vec512.xor (Vec512.repr (2^128 -1)) (Vec512.repr 0)))))).
+
+
+Compute (r_i_expr 0 (Vec512.repr 511) (Vec512.repr 0)). *)
 
 Definition gen_Ax : list (list int64) :=
   map (fun i =>
@@ -36,8 +46,10 @@ Definition gen_Ax : list (list int64) :=
              (seq 0 256))
       (seq 0 8).
 
+Definition Ax_vals := Ax_to_vals gen_Ax.
 
-(* Definition Spec_streebog_xlps_VarSpecs : varspecs := (_Ax, (tarray (tarray tulong 256) 8))::nil. *)
+Compute (Znth 2 Ax_vals).
+
 
 Definition streebog_xlps_spec :=
   DECLARE _streebog_xlps
@@ -52,7 +64,7 @@ Definition streebog_xlps_spec :=
     SEP (data_at sh_r tuint512 (block512_to_vals x_content) x;
          data_at sh_r tuint512 (block512_to_vals y_content) y;
          data_at sh_w tuint512 (block512_to_vals data_content) data;
-         data_at sh_r (tarray (tarray tulong 256) 8) (Ax_to_vals gen_Ax) (gv _Ax))
+         data_at sh_r (tarray (tarray tulong 256) 8) Ax_vals (gv _Ax))
   POST [tvoid]
     PROP()
     (* LOCAL (gvars gv) *)
@@ -60,7 +72,7 @@ Definition streebog_xlps_spec :=
     SEP (data_at sh_r tuint512 (block512_to_vals x_content) x;
          data_at sh_r tuint512 (block512_to_vals y_content) y;
          data_at sh_w tuint512 (block512_to_vals (LPSX x_content y_content)) data;
-         data_at sh_r (tarray (tarray tulong 256) 8) (Ax_to_vals gen_Ax) (gv _Ax)).
+         data_at sh_r (tarray (tarray tulong 256) 8) Ax_vals (gv _Ax)).
 
 
 Definition inv_data_mem (sh :share) (x : val) (x_content : block512) : mpred :=
@@ -81,11 +93,11 @@ Fixpoint Z_to_xor_list (k : nat) (x y : Z) : list val :=
             (Z_to_xor_list k' (Z.shiftr x (Z.of_nat 64)) (Z.shiftr y (Z.of_nat 64)))
   end.
 
-Lemma r_i_eq : forall (i j : Z) (r1 r2 : block512),
+Lemma r_i_eq : forall (j : Z) (r1 r2 : block512),
   Zlength (block512_to_chunks r1) = 8 -> Zlength (block512_to_chunks r2) = 8 ->
-  r_i_expr i j r1 r2 = Znth j (Z_to_xor_list 8 (Vec512.unsigned r1) (Vec512.unsigned r2)).
+  r_i_expr j r1 r2 = Znth j (Z_to_xor_list 8 (Vec512.unsigned r1) (Vec512.unsigned r2)).
 Proof.
-  intros i j r1 r2 H1 H2.
+  (* intros i j r1 r2 H1 H2.
   (* intros i j r1 r2. *)
   unfold r_i_expr.
   unfold Z_to_xor_list.
@@ -98,7 +110,7 @@ Proof.
   rewrite <- !Z.shiftr_lxor.
   rewrite !Z.shiftr_shiftr.
   Search (Znth).
-  Search map.
+  Search map. *)
   (* induction j as [|j' Hj']. *)
   (* - simpl. *)
   (*   unfold block512_to_chunks in *. *)
@@ -132,18 +144,22 @@ Definition inv_data_mem2 (sh :share) (x : val) : mpred :=
   data_at_ sh tuint512 x.
 
 
+
 Lemma body_streebog_xlps :
   semax_body  Vprog [] f_streebog_xlps streebog_xlps_spec.
 Proof.
   start_function.
-
-  (* assert (Zlength (block512_to_chunks x_content) = 8) by reflexivity.
+    (* assert (Zlength (block512_to_chunks x_content) = 8) by reflexivity.
   assert (Zlength (block512_to_chunks y_content) = 8) by reflexivity. *)
   (* assert (Zlength (block512_to_int64s z_content) = 8) by reflexivity. *)
 
   do 24 forward.
+  (* Opaque Ax_vals.
 
-  Definition Ax_vals := Ax_to_vals gen_Ax.
+  remember (Znth 0 Ax_vals) as ax0.
+  remember (Znth 0 (block512_to_chunks (LPSX x_content y_content))) as chunk0.
+  pose (idx := Z.land chunk0 255).
+  pose (v := Znth idx ax0). *)
 
   deadvars!.
   forward_loop
@@ -152,14 +168,14 @@ Proof.
           sublist 0 i (block512_to_vals (LPSX x_content y_content)) =
           sublist 0 i (block512_to_vals data_content))
     LOCAL (temp _i (Vint (Int.repr i));
-            temp _r0 (r_i_expr i 0 x_content y_content);
-            temp _r1 (r_i_expr i 1 x_content y_content);
-            temp _r2 (r_i_expr i 2 x_content y_content);
-            temp _r3 (r_i_expr i 3 x_content y_content);
-            temp _r4 (r_i_expr i 4 x_content y_content);
-            temp _r5 (r_i_expr i 5 x_content y_content);
-            temp _r6 (r_i_expr i 6 x_content y_content);
-            temp _r7 (r_i_expr i 7 x_content y_content);
+            temp _r0 (r_i_expr 0 x_content y_content);
+            temp _r1 (r_i_expr 1 x_content y_content);
+            temp _r2 (r_i_expr 2 x_content y_content);
+            temp _r3 (r_i_expr 3 x_content y_content);
+            temp _r4 (r_i_expr 4 x_content y_content);
+            temp _r5 (r_i_expr 5 x_content y_content);
+            temp _r6 (r_i_expr 6 x_content y_content);
+            temp _r7 (r_i_expr 7 x_content y_content);
             gvars gv;
             temp _data data) 
     SEP (inv_data_mem sh_r x x_content; inv_data_mem sh_r y y_content; 
@@ -168,8 +184,8 @@ Proof.
          (* inv_data_mem_res sh_w i data x_content y_content data_content; *)
          data_at sh_r (tarray (tarray tulong 256) 8) Ax_vals (gv _Ax))
     ).
-  - forward.
-    entailer!.
+    - forward.
+    entailer!. normalize.
     Exists 0. 
     entailer!.
     split. 
@@ -191,9 +207,10 @@ Proof.
       -- abbreviate_semax. forward.
         * entailer!. admit.
         * entailer!. (*rewrite Znth_0_cons.*) admit. 
-        * forward. deadvars!. forward.
+        * forward.  deadvars!. forward.
           ** entailer!. admit.
-          ** admit.
+          ** unfold r_i_expr. Search (Z.land).
+          Search (upd_Znth). (*forward.*) admit.
       -- lia.
          
 
